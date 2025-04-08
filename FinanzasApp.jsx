1021d93685caf0176e6e147c34f5ee767f95b366
Mai1685 <maia.primo.mp@gmail.com>
2025-04-08 02:54:05 -0300

    Add files via upload

---
+++ FinanzasApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FinanzasApp() {
+  return (
+    <div>
+      <h1>Finanzas App</h1>
+      <p>Seguimiento diario de ingresos y gastos.</p>
+    </div>
+  );
+}
+
+export default FinanzasApp;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Finanzas App
+Una app simple de control financiero con React y Vite.
